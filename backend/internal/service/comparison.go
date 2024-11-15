c360acea42b3d7abc8c7146cf37d1051ac13e19b
torderonex <eligorko.info@yandex.ru>
2024-11-16 01:22:16 +0300

    tmp

--- backend/internal/service/comparison.go
+++ backend/internal/service/comparison.go
@@ -1,13 +1,13 @@
 package service
 
 import (
-	"github.com/fullstakilla/naimix-hackathon/backend/internal/webapi/sber"
-	"log/slog"
+	"github.com/fullstakilla/naimix-hackathon/backend/internal/entities"
 )
 
-func Test(str string) (string, error) {
-	gigachat := sber.New()
-	ll, err := gigachat.DoPrompt("Сколько рыбы в море?")
-	slog.Info(ll)
-	return ll, err
+func GetComparisonBetween2(taro1 any, taro2 any) (entities.ComparisonResult, error) {
+	panic("")
+}
+
+func GetTeamComparison() {
+	panic("")
 }
